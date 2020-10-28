5d5e6fd1d6e4e6bb15354b25c80355faebcdde82
dme86 <me.daniel.meier@gmail.com>
2020-10-28 14:13:00 +0100

    add names

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 resource "aws_internet_gateway" "default" {
   vpc_id = var.vpc_id
 
-  tags = var.tags
+  tags = merge(
+    {
+      "Name" = format("%s", var.vpc_name)
+    },
+    var.tags,
+  )
 
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "tags" {}
 variable "vpc_id" {
   type = string
 }
+
+variable "vpc_name" {
+  type    = string
+  default = "default"
+}
